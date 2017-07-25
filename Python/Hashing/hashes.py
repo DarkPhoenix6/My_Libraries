b4bd0af2ba60a3fa13c8630bc529bd5bf29a19b5
DarkPhoenix6 <chrisfedun031595@gmail.com>
2017-07-25 15:04:28 -0700

    add function(s): - aes_hasher - reverse update function(s): - aes_hash - aes_enc - aes_encryption

--- Python/Hashing/hashes.py
+++ Python/Hashing/hashes.py
@@ -23,12 +23,12 @@ class Crypto(object):
         self.decryptor = self.cipher.decryptor()
 
     @staticmethod
-    def aes_enc(plain_text, key, iv=b'2222222222222222', keysize=MISSING):
+    def aes_enc(plain_text, key, iv=b'2222222222222222', keysize=256):
         pt = bytes(Crypto.padder(binascii.a2b_qp(plain_text)))
-        if keysize != MISSING:
-            byte_key = bytes(Crypto.padder(binascii.a2b_qp(key, keysize)))
+        if len(bytes(Crypto.padder(binascii.a2b_qp(key), keysize))) != keysize:
+            byte_key = bytes(Crypto.padder(binascii.a2b_qp(key), keysize))
         else:
-            byte_key = bytes(Crypto.padder(binascii.a2b_qp(key)))
+            byte_key = bytes(Crypto.padder(binascii.a2b_qp(key), keysize))
         mode = modes.CBC(bytes(binascii.a2b_qp(iv)))
         cipher = Cipher(algorithms.AES(byte_key), mode, backend=backend)
         encryptor = cipher.encryptor()
@@ -64,21 +64,32 @@ class Crypto(object):
         return data
 
     @staticmethod
-    def aes_hash(plain_text, key=b'22222222222222220123456789012345', iv=b'2222222222222222', keysize=MISSING):
+    def aes_hash(plain_text, key=b'22222222222222220123456789012345', iv=b'2222222222222222', keysize=256):
         plain_text_array = bytearray(binascii.a2b_qp(plain_text))
         blocks = Crypto.split_to_blocks(plain_text_array)
         blocks[-1] = Crypto.padder(blocks[-1], 128)
-        cipher_text_blocks = [] * len(blocks)
-        for i in range(len(blocks)):
-            l = list_mod_gen(blocks, len(blocks), i)
-
+        cipher_text_blocks = [0] * len(blocks)
+        for i in range(0, len(blocks)):
+            l = [i for i in mod_gen(len(blocks), len(blocks), i)][0]
+            current_ptext = b''.join(blocks[int(i)] for i in l)
+            cipher_text_blocks[i], iv = Crypto.aes_hasher(current_ptext, key, iv, keysize)
+        hast_text = b''.join(cipher_text_blocks)
+        return hast_text
 
     @staticmethod
-    def aes_encryption(plain_text, key=b'22222222222222220123456789012345', iv=b'2222222222222222', keysize=MISSING):
+    def aes_hasher(data, key, iv, keysize):
+        cipher_text = Crypto.aes_enc(data, key, iv, keysize)
+        iv2 = Crypto.split_to_blocks(cipher_text, 128)
+        return iv2, iv2
+
+    @staticmethod
+    def aes_encryption(plain_text, key=b'22222222222222220123456789012345', iv=b'2222222222222222', keysize=256):
         plain_text_array = bytearray(binascii.a2b_qp(plain_text))
         blocks = Crypto.split_to_blocks(plain_text_array)
         blocks[-1] = Crypto.padder(blocks[-1], 128)
         padded_plain_text = b''.join(blocks)
+        ciphertext = Crypto.aes_enc(padded_plain_text, key, iv, keysize)
+        return ciphertext
 
     @staticmethod
     def split_to_blocks(plain_text_array, blocksize=128):
@@ -115,6 +126,11 @@ class Crypto(object):
         result = number ^ mask
         return bin(result)
 
+    @staticmethod
+    def reverse(data):
+        data2 = data[len(data) - 1:-1:-1]
+        return data2
+
 
 class TestCrypto(unittest.TestCase):
     """ Basic unit tests for Rectangle"""
@@ -123,11 +139,16 @@ class TestCrypto(unittest.TestCase):
         """ Tests the generation of a roof quadrilateral """
         key = b'u\x1eE\xd6\x9bn\x0b\x82\x13\r\xf5:\xc2Ii\xb7K\xa9_\xd3N\x10V\x17\xfb\x1e\xd9\xa7\xa9.\x0b\xe4'
 
-
     def test_block_split(self):
         blocks = Crypto.split_to_blocks(
             b'passwordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpasswordpassword')
 
+    def test_aes_hash(self):
+        key = b'u\x1eE\xd6\x9bn\x0b\x82\x13\r\xf5:\xc2Ii\xb7K\xa9_\xd3N\x10V\x17\xfb\x1e\xd9\xa7\xa9.\x0b\xe4'
+        pt = b'passwordqasswordsasswordmasswordbasswordtasswordcassworddasswordfasswordwasswordvassword'
+        iv = b'2222222222222222'
+        ct = Crypto.aes_hash(pt, key, iv, 256)
+        print(ct)
 
 # Won't run if imported
 if __name__ == "__main__":
--- Python/ModGen/mod_gen.py
+++ Python/ModGen/mod_gen.py
@@ -2,7 +2,6 @@ from __future__ import division
 from MultiMethod.multimethod import multimethod
 
 
-@multimethod(int, int, int)
 def mod_gen(n, m, s=0):
     """
     Generator function that yields n tuples of m length containing consecutive indices within bounds n
@@ -16,22 +15,6 @@ def mod_gen(n, m, s=0):
         yield(tuple([(i + k + s) % n for k in range(int(m/2)-m+1, int(m/2+1))]))
 
 
-@multimethod(int, int, int, int)
-def mod_gen(n, m, num, s=0):
-    """
-    Generator function that yields n tuples of m length containing consecutive indices within bounds n
-    Such that the set of tuples generated from mod_gen(3, 3) would be ((2, 0, 1), (0, 1, 2), (1, 2, 0))
-    :param n: The bounds of the indices in the tuple
-    :param m: The number of indices per tuple
-    :param num: The number of tuples to generate
-    :param s: The starting index, defaults to 0
-    :return: Yields the next tuple in the set
-    """
-    for i in range(num):
-        yield(tuple([(i + k + s) % n for k in range(int(m/2)-m+1, int(m/2+1))]))
-
-
-@multimethod(object, int, int)
 def list_mod_gen(iterable, m, s=0, **num):
     """ Generator function that yields n tuples of m length containing consecutive list elements within bounds n
     Such that the set of tuples generated from list_mod_gen([a, b, c], 3) would be ((c, a, b), (a, b, c), (b, c, a))
