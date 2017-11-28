9ea63a29fb1e47cee4c4de9b41c4ed0124aaebf7
Chris Fedun <chrisfedun031595@gmail.com>
2017-11-28 13:40:51 -0800

    Update mod_gen.py

--- Python/ModGen/mod_gen.py
+++ Python/ModGen/mod_gen.py
@@ -13,8 +13,22 @@ def mod_gen(n, m, s=0):
     for i in range(n):
         yield(tuple([(i + k + s) % n for k in range(int(m/2)-m+1, int(m/2+1))]))
 
+        
+def corrected_mod_gen(b, m, s=0, n=1):
+    """
+    Generator function that yields n tuples of m length containing consecutive indices within bounds n
+    Such that the set of tuples generated from corrected_mod_gen(3, 3) would be ((0, 1, 2), (1, 2, 0), (2, 0, 1))
+    :param b: The bounds of the indices in the tuple
+    :param m: The number of indices per tuple
+    :param s: The starting index, defaults to 0
+    :param b: The number of tuples to generate
+    :return: Yields the next tuple in the set
+    """
+    for i in range(n):
+        yield(tuple([(i + k + s) % n for k in range(m)]))
 
-def list_mod_gen(iterable, m, s=0, **num):
+
+def list_mod_gen(iterable, m, s=0):
     """ Generator function that yields n tuples of m length containing consecutive list elements within bounds n
     Such that the set of tuples generated from list_mod_gen([a, b, c], 3) would be ((c, a, b), (a, b, c), (b, c, a))
     :param iterable: An iterable object with which we wish to iterate over consecutive elements
