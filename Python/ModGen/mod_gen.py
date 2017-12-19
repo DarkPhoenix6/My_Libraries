c302e83d90aa62d39765d1c4192a8711865be138
Chris Fedun <chrisfedun031595@gmail.com>
2017-12-19 13:55:15 -0800

    documentation

--- Python/ModGen/mod_gen.py
+++ Python/ModGen/mod_gen.py
@@ -14,18 +14,18 @@ def mod_gen(n, m, s=0):
         yield(tuple([(i + k + s) % n for k in range(int(m/2)-m+1, int(m/2+1))]))
 
         
-def corrected_mod_gen(b, m, s=0, n=1):
+def corrected_mod_gen(bounds, m, start=0, n=1):
     """
     Generator function that yields n tuples of m length containing consecutive indices within bounds n
     Such that the set of tuples generated from corrected_mod_gen(3, 3) would be ((0, 1, 2), (1, 2, 0), (2, 0, 1))
-    :param b: The bounds of the indices in the tuple
+    :param bounds: The bounds of the indices in the tuple
     :param m: The number of indices per tuple
-    :param s: The starting index, defaults to 0
+    :param start: The starting index, defaults to 0
     :param n: The number of tuples to generate
     :return: Yields the next tuple in the set
     """
     for i in range(n):
-        yield(tuple([(i + k + s) % b for k in range(m)]))
+        yield(tuple([(i + k + start) % bounds for k in range(m)]))
 
 
 def list_mod_gen(iterable, m, s=0):
