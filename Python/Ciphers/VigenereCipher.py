02c0c8e798afcc0367ad95521e4c78fb65292723
Chris Fedun <chrisfedun031595@gmail.com>
2018-01-15 12:10:41 -0800

    Add CrackVigenere fix encrypt/decrypt bug

--- Python/Ciphers/VigenereCipher.py
+++ Python/Ciphers/VigenereCipher.py
@@ -21,6 +21,7 @@ standard_library.install_aliases()
 
 
 class VigenereCipher(object):
+
     @staticmethod
     def encrypt(message, key):
         return VigenereCipher.translate_message(message, key, 'encrypt')
@@ -36,6 +37,7 @@ class VigenereCipher(object):
         i = 0   # key Index
         key = key.upper()
         len1 = len(alphabet)
+        key_len = len(key)
         for j in message:  # loop through each character in the message
             num = VigenereCipher.find_symbol(alphabet, j)
             if num != -1:  # -1 means j.upper() was not found in alphabet
@@ -51,7 +53,7 @@ class VigenereCipher(object):
                 elif j.islower():
                     translated.append(alphabet[num].lower())
                 i += 1
-                if i == len1:
+                if i == key_len:
                     i = 0
             else:
                 # The character was not in alphabet, so add it to translated as it is.
@@ -63,5 +65,47 @@ class VigenereCipher(object):
         return alphabet.find(j.upper())
 
 
-class CrackVigenere(VigenereCipher):
+class CrackVigenere(object):
+    @staticmethod
+    def dictionary_attack(cipher_text):
+        encryption_break_dict = {}
+        detect_english = DetectEnglish()
+        for w in detect_english.english_dict:
+            decrypted_text = VigenereCipher.decrypt(cipher_text, w)
+            if detect_english.is_english(decrypted_text, word_percent=40):
+                CrackVigenere.add_to_enc_break_dict(encryption_break_dict, w, decrypted_text)
+        return encryption_break_dict
 
+    @staticmethod
+    def add_to_enc_break_dict(break_dict, key, dec_message):
+        break_dict[key] = dec_message[:100]
+
+m = 'tkg kcl sgzgj gzgs hggs pn lncsgt bvyy fspvy cqpgj kgj qcpkgj csl bnpkgj lvgl, pknfuk tvw nj gvukp pvbgt c ' \
+       'egcj tkg ogsp pn pnos ns tcpfjlce, vs pkg ocuns, vs c bcvy-njlgj ljgtt csl kgj hcjg qggp qycp vs pkg ocuns ' \
+       'hgl csl kgj tkngt ojcaagl vs c avgig nq acagj hgtvlg kgj ns pkg tgcp. tkg onfyl afp ns pkg tkngt xftp ' \
+       'hgqnjg pkg ocuns jgcikgl pnos. cqpgj tkg unp pn hg c hvu uvjy tkg onfyl ctm kgj qcpkgj pn tpna pkg ocuns cp ' \
+       'pkg glug nq pnos csl tkg onfyl ugp lnos csl ocym. tkg onfyl snp pgyy kgj qcpkgj oke tkg ocspgl pn ocym vs ' \
+       'vstpgcl nq jvlvsu. kg pknfukp pkcp vp oct hgicftg nq pkg tbnnpk tpjggpt, pkg tvlgocymt. hfp vp oct hg icftg ' \
+       'tkg hgyvgzgl pkcp pkg agnayg okn tco kgj csl oknb tkg acttgl ns qnnp onfyl hgyvgzg pkcp tkg yvzgl vs pkg ' \
+       'pnos pnn. okgs tkg oct pogyzg egcjt nyl kgj qcpkgj csl bnpkgj lvgl vs pkg tcbg tfbbgj, vs c ynu knftg nq ' \
+       'pkjgg jnnbt csl c kcyy, ovpknfp tijggst, vs c jnnb yvukpgl he c hfutovjygl mgjntgsg ycba, pkg scmgl qynnj ' \
+       'onjs tbnnpk ct nyl tvyzgj he scmgl qggp. tkg oct pkg enfsugtp yvzvsu ikvyl. kgj bnpkgj lvgl qvjtp. tkg tcvl, ' \
+       'pcmg icjg nq aco. ygsc lvl tn. pkgs nsg lce kgj qcpkgj tcvl, enf un pn lncsgt bvyy ovpk bimvsyge. enf ugp ' \
+       'jgcle pn un, hg jgcle okgs kg inbgt. pkgs kg lvgl. bimvsyge, pkg hjnpkgj, cjjvzgl vs c ocuns. pkge hfjvgl ' \
+       'pkg qcpkgj vs c ujnzg hgkvsl c infspje ikfjik nsg cqpgjsnns, ovpk c avsg kgcltpnsg. pkg sgwp bnjsvsu tkg ' \
+       'lgacjpgl qnjgzgj, pknfuk vp vt anttvhyg pkcp tkg lvl snp msno pkvt cp pkg pvbg, vs pkg ocuns ovpk bimvsyge, ' \
+       'qnj lncsgt bvyy. pkg ocuns oct hnjjnogl csl pkg hjnpkgj kcl ajnbvtgl pn jgpfjs vp he svukpqcyy. pkg hjnpkgj ' \
+       'onjmgl vs pkg bvyy. cyy pkg bgs vs pkg zvyycug. onjmgl vs pkg bvyy nj qnj vp. vp oct ifppvsu avsg. vp kcl ' \
+       'hggs pkgjg tgzgs egcjt csl vs tgzgs egcjt bnjg vp onfyl lgtpjne cyy pkg pvbhgj ovpkvs vpt jgcik. pkgs tnbg ' \
+       'nq pkg bcikvsgje csl bntp nq pkg bgs okn jcs vp csl gwvtpgl hgicftg nq csl qnj vp onfyl hg ynclgl nspn ' \
+       'qjgvukp icjt csl bnzgl coce. hfp tnbg nq pkg bcikvsgje onfyl hg ygqp, tvsig sgo avgigt infyl cyocet hg ' \
+       'hnfukp ns pkg vstpcyybgsp aycs-ucfsp, tpcjvsu, bnpvnsygtt okggyt jvtvsu qjnb bnfslt nq hjvim jfhhyg csl ' \
+       'jcuugl ogglt ovpk c rfcyvpe ajnqnfslye ctpnsvtkvsu, csl ufppgl hnvygjt yvqpvsu pkgvj jftpvsu csl fstbnmvsu ' \
+       'tpcimt ovpk cs cvj tpfhhnjs, hcqqygl csl hgbftgl fans c tpfbaanimgl tigsg nq ajnqnfsl csl agcigqfy ' \
+       'lgtnycpvns, fsaynogl, fspvyygl, ufppvsu tynoye vspn jgl csl iknmgl jczvsgt hgsgcpk pkg ynsu rfvgp jcvst nq ' \
+       'cfpfbs csl pkg ucyynavsu qfje nq zgjscy grfvsnwgt. pkgs pkg kcbygp okvik cp vpt hgtp lce kcl hnjsg sn scbg ' \
+       'yvtpgl ns antpnqqvig lgacjpbgsp csscyt onfyl snp sno gzgs hg jgbgbhgjgl he pkg knnmonjbjvllgs kgvjtcp-ycjug ' \
+       'okn afyygl pkg hfvylvsut lnos csl hfjvgl pkgb vs innmtpnzgt csl ovspgj ujcpgt. '
+
+crack_dict = CrackVigenere.dictionary_attack(m)
+print(crack_dict)
